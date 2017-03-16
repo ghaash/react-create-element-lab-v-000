8a3c73d213279d37e644a5dc5743304bb3160668
ghaash <daniel.silverman@me.com>
2017-03-16 11:29:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 //Code React element here
+let who = React.createElement(
+  'h1',
+  {},
+  'An Awesome Person'
+);
+
+let learn = React.createElement(
+  'p',
+  {},
+  'Who is learning React'
+);
+
+let interests = React.createElement(
+  "ul",
+  {className: "me__interests"},
+  [
+    React.createElement("li",{},"JavaScript"),
+    React.createElement("li",{},"React"),
+    React.createElement("li",{},"Movies"),
+    React.createElement("li",{},"Ice cream")
+  ]
+);
+
+let meInReact = React.createElement(
+  'div',
+  {className: 'me'},
+  [who, learn, interests]
+)
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+);
